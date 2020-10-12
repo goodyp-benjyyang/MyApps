ad1b6de3fc0b9bbc6bab3d5ad5cd253c880e8642
goodyp-benjyyang <goodyp12323@qq.com>
2020-10-12 22:46:17 +0800

    record imformation into properties files

--- src/main/java/top/goodyp/springboot/controller/AuthorizeController.java
+++ src/main/java/top/goodyp/springboot/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package top.goodyp.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,20 +14,27 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setRedirect_uri("http://localhost:8899/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("193299d96df4b9e72db1");
-        accessTokenDTO.setClient_secret("6da7cc20ee647d62fc53fd1f9300d78c813fbb29");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken( accessTokenDTO );
 
 
         GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println( user.toString() );
         System.out.println( user.getName() );
         return "index";
     }
